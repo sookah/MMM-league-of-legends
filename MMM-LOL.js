26cc9d443e73e660b0b2a43308ec4010bb4f04cc
Saujan Ghimire <supertrampsookah@gmail.com>
2019-11-22 18:49:10 +0100

    add table

--- MMM-LOL.js
+++ MMM-LOL.js
@@ -2,14 +2,53 @@ Module.register("MMM-LOL", {
   // Default module config.
   defaults: {
     title: "LOL Client",
-    username: "supertrampsookah"
+    username: "supertrampsookah",
+    apiKey: "",
   },
 
   // Override dom generator.
   getDom: function () {
     var wrapper = document.createElement("div");
     wrapper.innerHTML = this.config.title;
-    return wrapper;
+
+    if (this.config.apiKey === "") {
+      wrapper.innerHTML = "Please set the correct LOL api key for module: " + this.name + ".";
+      wrapper.className = "dimmed light small";
+      return wrapper;
+    }
+
+    // if (!this.loaded) {
+    //     wrapper.innerHTML = this.translate("LOADING");
+    //     wrapper.className = "dimmed light small";
+    //     return wrapper;
+    // }
+
+    var table = document.createElement("table");
+    table.className = this.config.tableClass;
+
+    friends = ['Eternal Phase',
+      'jrKarki',
+      'Dark Knight64',
+      'Doctor Player',
+      'superbatman13'
+    ]
+
+    for (var f in friends) {
+      // var forecast = this.forecast[f];
+
+      var row = document.createElement("tr");
+      if (this.config.colored) {
+        row.className = "colored";
+      }
+      table.appendChild(row);
+
+      var nameCell = document.createElement("td");
+      nameCell.className = "username";
+      nameCell.innerHTML = friends[f];
+      row.appendChild(nameCell);
+    }
+
+    return table;
   },
 
   getToken: function () {
@@ -20,4 +59,8 @@ Module.register("MMM-LOL", {
   getStyles: function () {
     return ["lol.css"];
   },
+
+  start: function () {
+
+  }
 });
