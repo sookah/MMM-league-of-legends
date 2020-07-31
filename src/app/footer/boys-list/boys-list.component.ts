6b7749a157e0d9877f2b55d51f8bf5c22a320683
Saujan Ghimire <supertrampsookah@gmail.com>
2020-07-31 21:07:28 +0100

    feat(boys): add boys component

---
+++ src/app/footer/boys-list/boys-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boys-list',
+  templateUrl: './boys-list.component.html',
+  styleUrls: ['./boys-list.component.scss']
+})
+export class BoysListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
