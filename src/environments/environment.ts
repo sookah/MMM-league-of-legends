d504955a14aeb4ee8fa6e72d0fa3340280f8ff81
Saujan Ghimire <supertrampsookah@gmail.com>
2020-08-01 15:20:25 +0100

    feat(api): add faker and json-server for fake data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  RIOT_API_DEV_KEY: 'RGAPI-68795120-5d9a-4d55-87f6-8f1f9fc38ef6'
+  RIOT_API_DEV_KEY: ''
 };
+
+// TODO: add secret keys to env
 
 /*
  * For easier debugging in development mode, you can import the following file
