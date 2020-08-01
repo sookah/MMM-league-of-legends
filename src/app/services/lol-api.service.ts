e012d8e0750cbe653837f52349f62d610f3cd812
Saujan Ghimire <supertrampsookah@gmail.com>
2020-08-01 18:15:10 +0100

    feat(riotAPI): add get summoner info by name

--- src/app/services/lol-api.service.ts
+++ src/app/services/lol-api.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Summoner } from '../shared/summoner';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LolApiService {
 
-  constructor() { }
+  private RIOT_API_URL = 'https://euw1.api.riotgames.com';
+  private RIOT_SUMMONER_URL = 'lol/summoner/v4/summoners';
+  constructor(private http: HttpClient) { }
+
+  getSummonerInfo(summoner: Summoner): {} {
+    return this.http.get(`${this.RIOT_API_URL}/${this.RIOT_SUMMONER_URL}/by-name/${summoner.name}?api_key=${environment.RIOT_API_DEV_KEY}`);
+  }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  RIOT_API_DEV_KEY: ''
+  RIOT_API_DEV_KEY: 'RGAPI-68795120-5d9a-4d55-87f6-8f1f9fc38ef6'
 };
 
 // TODO: add secret keys to env
