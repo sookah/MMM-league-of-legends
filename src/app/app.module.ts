64519025ff3d24a4c9f298ab87a4eec4dd0ad563
Saujan Ghimire <supertrampsookah@gmail.com>
2020-08-01 08:56:04 +0100

    feat(app): add components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BoysListComponent } from './footer/boys-list/boys-list.component';
+// import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BoysListComponent
   ],
   imports: [
     BrowserModule,
